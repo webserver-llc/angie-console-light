70be7306fb70428cf5ee4bdd86bedd38f260ab1c
Kirill Malyakin <kirill.malyakin@wbsrv.ru>
2023-08-25 15:22:05 +0300

    add default port for server prop in upstream

--- src/api/upstreamsApi/__test__/utils.test.js
+++ src/api/upstreamsApi/__test__/utils.test.js
@@ -6,11 +6,36 @@ describe('UpstreamsApi - Utils', () => {
     expect(() => getServerName({})).to.throw(/required/);
     expect(() => getServerName({ server: null })).to.throw(/required/);
     expect(() => getServerName({ server: undefined })).to.throw(/required/);
-    expect(() => getServerName({ server: '', service: '' })).to.throw(/required/);
-    expect(getServerName({ server: '10.0.0.0:80' })).to.be.string('10.0.0.0:80');
-    expect(getServerName({ server: '10.0.0.0:80', service: undefined })).to.be.string('10.0.0.0:80');
-    expect(getServerName({ server: '10.0.0.0:80', service: null })).to.be.string('10.0.0.0:80');
-    expect(getServerName({ server: '10.0.0.0:80', service: '' })).to.be.string('10.0.0.0:80');
-    expect(getServerName({ server: '10.0.0.0:80', service: '_http._tcp' })).to.be.string('_http._tcp@10.0.0.0:80');
-  })
-})
+    expect(() => getServerName({ server: '', service: '' })).to.throw(
+      /required/,
+    );
+    expect(getServerName({ server: 'backend.server.com' })).to.be.string(
+      'backend.server.com',
+    );
+    expect(getServerName({ server: '10.0.0.0' })).to.be.string('10.0.0.0:80');
+    expect(getServerName({ server: '10.0.0.0:8080' })).to.be.string(
+      '10.0.0.0:8080',
+    );
+    expect(getServerName({ server: '10.0.0.0:80' })).to.be.string(
+      '10.0.0.0:80',
+    );
+    expect(
+      getServerName({ server: '10.0.0.0:80', service: undefined }),
+    ).to.be.string('10.0.0.0:80');
+    expect(
+      getServerName({ server: '10.0.0.0:80', service: null }),
+    ).to.be.string('10.0.0.0:80');
+    expect(getServerName({ server: '10.0.0.0:80', service: '' })).to.be.string(
+      '10.0.0.0:80',
+    );
+    expect(
+      getServerName({ server: '10.0.0.0:80', service: '_http._tcp' }),
+    ).to.be.string('_http._tcp@10.0.0.0:80');
+    expect(
+      getServerName({ server: '10.0.0.0', service: '_http._tcp' }),
+    ).to.be.string('_http._tcp@10.0.0.0:80');
+    expect(
+      getServerName({ server: 'backend.server.com', service: '_http._tcp' }),
+    ).to.be.string('_http._tcp@backend.server.com');
+  });
+});
--- src/api/upstreamsApi/utils.js
+++ src/api/upstreamsApi/utils.js
@@ -1,5 +1,11 @@
+import { isIP } from '../../utils';
+
+const defaultPort = 80;
+
 export const getServerName = (peer) => {
-	if (!peer) { throw new Error('Peer is required argument and should be an object'); }
+	if (!peer) {
+		throw new Error('Peer is required argument and should be an object');
+	}
 
 	if (
 		!('server' in peer) ||
@@ -9,13 +15,21 @@ export const getServerName = (peer) => {
 		throw new Error('Server is required in peer');
 	}
 
+	// add default port, if isn't set
+	// should fix next version Angie Pro
+	function getServer(server) {
+		if (isIP(server) && server.indexOf(':') === -1)
+			return `${server}:${defaultPort}`;
+		return server;
+	}
+
 	if ('service' in peer) {
 		if (typeof peer.service === 'string' && peer.service.length !== 0) {
-			return `${peer.service}@${peer.server}`;
+			return `${peer.service}@${getServer(peer.server)}`;
 		}
 	}
 
-	return peer.server;
+	return getServer(peer.server);
 };
 
 export default {
